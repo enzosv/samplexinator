897077fd5a1b753dacb59ca8b6940e01ac85f345
enzosv <vergara.enzo@gmail.com>
2025-03-29 23:53:51 +0800

    add category to q in attempt

--- attempt.js
+++ attempt.js
@@ -18,31 +18,31 @@ async function renderAttempt() {
     (${new Date(attempt.timestamp).toLocaleString()})`;
 
   let score = 0;
-  const container = document.getElementById("attempt-container");
-  container.innerHTML = "";
-
-  // Fetch questions data
   const response = await fetch(`./questions.json`);
-  const data = await response.json();
-  let allQuestions = [];
-  data.forEach((category) => {
-    allQuestions = [...allQuestions, ...category];
-  });
+  const allQuestions = await response.json();
   let questions = [];
+  const container = document.getElementById("attempt-container");
 
   attempt.answers &&
     Object.entries(attempt.answers).forEach(([questionId, userAnswer]) => {
-      const q = allQuestions.find((q) => q.id == questionId);
+      let q;
+      Object.keys(allQuestions).forEach((category) => {
+        const found = allQuestions[category].find(
+          (q) => q.id == parseInt(questionId)
+        );
+        if (found) {
+          q = found;
+          q.category = category;
+        }
+      });
       if (!q) {
         console.error("Question not found:", questionId);
         return;
       }
       questions.push(q);
-
       const div = document.createElement("div");
       div.className = "question border p-3 mb-3 rounded";
       div.innerHTML = `<p><strong>${q.question}</strong></p>`;
-
       for (const [key, value] of Object.entries(q.options)) {
         const isUserAnswer = userAnswer === key;
         const isCorrect = q.correct_answer === key;
@@ -65,12 +65,10 @@ async function renderAttempt() {
                         </div>
                     `;
       }
-
       const correct = userAnswer == q.correct_answer;
       if (correct) {
         score++;
       }
-
       div.innerHTML += `<p class="fw-bold ${
         correct ? "text-success" : "text-danger"
       }">Your Answer: ${letters[userAnswer] || "No answer"} | Correct Answer: ${
@@ -78,25 +76,20 @@ async function renderAttempt() {
       }</p>`;
       container.appendChild(div);
     });
-
   const numQuestions = questions.length;
   const scorePercentage = (score / numQuestions) * 100;
   const scoreContainer = document.getElementById("score-breakdown");
   let scoreBreakdownText = `<h4>Score: ${score} / ${numQuestions} (${scorePercentage.toFixed(
     2
   )}%)</h4>`;
-
   const categoryCounts = { anatomy: 0, physics: 0, procedures: 0 };
   let categoryScores = { anatomy: 0, physics: 0, procedures: 0 };
-
   questions.forEach((q) => {
     if (attempt.answers && attempt.answers[q.id] == q.correct_answer) {
       categoryScores[q.category]++;
     }
     categoryCounts[q.category]++;
   });
-
-  scoreBreakdownText += "<h5>Category Breakdown:</h5>";
   for (const category in categoryCounts) {
     const correct = categoryScores[category];
     const total = categoryCounts[category];
@@ -105,7 +98,6 @@ async function renderAttempt() {
       2
     )}%)</p>`;
   }
-
   scoreContainer.innerHTML = scoreBreakdownText;
 }
 renderAttempt();
