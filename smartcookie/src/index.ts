75e052e92bc6c334bb7c3184a03f52d97046bf60
enzosv <vergara.enzo@gmail.com>
2025-04-18 14:31:20 +0800

    use auth for get and save quiz

--- smartcookie/src/index.ts
+++ smartcookie/src/index.ts
@@ -58,11 +58,6 @@ async function handleJSONRequest(request: Request, env: Env) {
 	}
 	if (request.method == 'POST' && url.pathname == '/api/quiz') {
 		// save quiz
-		const { data: auth, error: authError } = await tryCatch(verifyAuth(request));
-		if (authError) {
-			return authError;
-		}
-		return auth;
 
 		// parse body
 		const answers = (await request.json()) as Answer[];
@@ -70,18 +65,29 @@ async function handleJSONRequest(request: Request, env: Env) {
 			return { error: 'Invalid request body - expected array of answers', status: 400 };
 		}
 
-		const { data: quiz_id, error: err } = await tryCatch(saveQuiz(env.DB, 1, answers));
+		// auth
+		const { data: auth, error: authError } = await tryCatch(verifyAuth(request));
+		if (authError) {
+			return authError;
+		}
+
+		const { data: quiz_id, error: err } = await tryCatch(saveQuiz(env.DB, auth.user_id, answers));
 		if (err) {
 			throw err;
 		}
-		return { quiz_id: quiz_id };
+		return queryQuiz(env.DB, quiz_id, auth.user_id);
 	}
 	if (request.method == 'GET' && url.pathname.startsWith('/api/quiz/')) {
 		const id = parseInt(url.pathname.split('/').pop() || '', 10);
 		if (isNaN(id)) {
 			return { error: 'Invalid quiz ID', status: 400 };
 		}
-		return queryQuiz(env.DB, id);
+		// auth
+		const { data: auth, error: authError } = await tryCatch(verifyAuth(request));
+		if (authError) {
+			return authError;
+		}
+		return queryQuiz(env.DB, id, auth.user_id);
 	}
 	return { error: 404 };
 }
@@ -129,8 +135,16 @@ function initDB(db: D1Database): Promise<D1Result<unknown>[]> {
 	]);
 }
 
-function queryQuiz(db: D1Database, quiz_id: number) {
-	return db.prepare(`SELECT * FROM answers WHERE quiz_id = ?;`).bind(quiz_id).all();
+async function queryQuiz(db: D1Database, quiz_id: number, user_id: number) {
+	const quiz = await db.prepare(`SELECT quiz_id FROM quizzes WHERE user_id = ?`).bind(user_id).first();
+	if (!quiz) {
+		throw { error: 404 };
+	}
+	const answers = await db.prepare(`SELECT question_id, choice FROM answers WHERE quiz_id = ?;`).bind(quiz_id).all();
+	if (answers.length < 1) {
+		throw { error: 404 };
+	}
+	return answers.results;
 }
 
 async function saveQuiz(db: D1Database, user_id: number, answers: Answer[]): Promise<number> {
@@ -170,5 +184,5 @@ async function saveQuizOnly(db: D1Database, user_id: number): Promise<number> {
 	if (!result) {
 		throw 'invalid result';
 	}
-	return result;
+	return result.quiz_id;
 }
